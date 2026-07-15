0a8f151ae06449a3e78dc31b9893ad46f3e69f24
Rakshit Singh <rakshitsingh0612@gmail.com>
2026-07-16 00:04:04 +0530

    learning and dping problems

---
+++ problem1.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+    #include<windows.h>
+    #include<stdlib.h>
+
+
+
+    int main ()
+    
+    {  for ( int i=1 ; i<=50 ; i += 2)
+    
+
+   { printf(" %d" , i); } 
+
+        return 0;
+    }
